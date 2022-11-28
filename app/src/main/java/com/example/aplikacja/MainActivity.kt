861afbf04194826177fd6fc8ccdffb40544e64f2
PC <julia.jasiewicz02@gmail.com>
2022-11-28 18:12:45 +0100

    a

---
+++ app/src/main/java/com/example/aplikacja/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.aplikacja
+
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bLeft = findViewById<Button>(R.id.wlewo)
+        val bRight = findViewById<Button>(R.id.wprawo)
+        val res = findViewById<Button>(R.id.submit)
+        val images = arrayOf(R.drawable.pies, R.drawable.pies2, R.drawable.pies3)
+        val image = findViewById<ImageView>(R.id.imageView)
+        bLeft.setOnClickListener() {
+            if (image.drawable.constantState == resources.getDrawable(images[0]).constantState) {
+                image.setImageResource(images[2])
+            } else if (image.drawable.constantState == resources.getDrawable(images[1]).constantState) {
+                image.setImageResource(images[0])
+            } else if (image.drawable.constantState == resources.getDrawable(images[2]).constantState) {
+                image.setImageResource(images[1])
+            }
+        }
+        bRight.setOnClickListener() {
+            if (image.drawable.constantState == resources.getDrawable(images[0]).constantState) {
+                image.setImageResource(images[1])
+            } else if (image.drawable.constantState == resources.getDrawable(images[1]).constantState) {
+                image.setImageResource(images[2])
+            } else if (image.drawable.constantState == resources.getDrawable(images[2]).constantState) {
+                image.setImageResource(images[0])
+            }
+        }
+        res.setOnClickListener() {
+            val text = findViewById<EditText>(R.id.silnia)
+            val number = text.text.toString().toInt()
+            val result = number*1*2*3
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Result")
+            builder.setMessage("Silnia z liczby $number wynosi: $result")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        }
+
+    }
+}
