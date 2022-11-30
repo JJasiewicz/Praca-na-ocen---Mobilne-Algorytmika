cf834acb7b65c3a9221246bd09785e48f7fa571b
PC <julia.jasiewicz02@gmail.com>
2022-11-30 19:45:55 +0100

    a

--- app/src/main/java/com/example/aplikacja/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aplikacja
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +8,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,21 +20,29 @@ class MainActivity : AppCompatActivity() {
         val images = arrayOf(R.drawable.pies, R.drawable.pies2, R.drawable.pies3)
         val image = findViewById<ImageView>(R.id.imageView)
         bLeft.setOnClickListener() {
-            if (image.drawable.constantState == resources.getDrawable(images[0]).constantState) {
-                image.setImageResource(images[2])
-            } else if (image.drawable.constantState == resources.getDrawable(images[1]).constantState) {
-                image.setImageResource(images[0])
-            } else if (image.drawable.constantState == resources.getDrawable(images[2]).constantState) {
-                image.setImageResource(images[1])
+            when (image.drawable.constantState) {
+                resources.getDrawable(images[0]).constantState -> {
+                    image.setImageResource(images[2])
+                }
+                resources.getDrawable(images[1]).constantState -> {
+                    image.setImageResource(images[0])
+                }
+                resources.getDrawable(images[2]).constantState -> {
+                    image.setImageResource(images[1])
+                }
             }
         }
         bRight.setOnClickListener() {
-            if (image.drawable.constantState == resources.getDrawable(images[0]).constantState) {
-                image.setImageResource(images[1])
-            } else if (image.drawable.constantState == resources.getDrawable(images[1]).constantState) {
-                image.setImageResource(images[2])
-            } else if (image.drawable.constantState == resources.getDrawable(images[2]).constantState) {
-                image.setImageResource(images[0])
+            when (image.drawable.constantState) {
+                resources.getDrawable(images[0]).constantState -> {
+                    image.setImageResource(images[1])
+                }
+                resources.getDrawable(images[1]).constantState -> {
+                    image.setImageResource(images[2])
+                }
+                resources.getDrawable(images[2]).constantState -> {
+                    image.setImageResource(images[0])
+                }
             }
         }
         res.setOnClickListener() {
